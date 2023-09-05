facb2508cbfaa7d20a556fffead6d7ba94ad7c80
CarlArgente <53144043+CarlArgente@users.noreply.github.com>
2023-09-05 19:11:28 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Display from "./Display";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  state = {
+    fullName: "Carl Emerson Llamera Argente",
+    age: 23,
+    position: "Azure DevOps Trainee",
+    contactNumber: "0945-866-2584",
+    coWorkers: [
+      {
+        id: 1,
+        name: "Eldrin Brylle Latayan",
+      },
+      {
+        id: 2,
+        name: "Enrico Emil Dela Rosa",
+      },
+      {
+        id: 3,
+        name: "Paolo Barbosa",
+      },
+      {
+        id: 4,
+        name: "Ryan Rasuman",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        <h1>Carl Emerson Llamera Argente | Week 1 - Day - 1 | Assignment</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, my name is {this.state.fullName} with the age of{" "}
+          {this.state.age}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>
+          My position name is {this.state.position} and my contact number is{" "}
+          {this.state.contactNumber}
+        </p>
+        <Display coWorkers={this.state.coWorkers} />
+      </div>
+    );
+  }
 }
-
 export default App;
+
+//rsc + enter function base component
+//rcc + enter class base component
